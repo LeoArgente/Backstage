2071a514eaae572d0a616c63426589292a1d95ac
henriq-az <caladoh8@gmail.com>
2025-09-22 15:25:38 -0300

    Class Filme transferida para o app backstage e app filmes deletado

--- backstage/models.py
+++ backstage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    ano = models.IntegerField()
+    descricao = models.TextField()
+    poster = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
